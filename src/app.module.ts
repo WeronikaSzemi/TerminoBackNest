8034539787753000fe106ed5daa518b5cb5e77bf
WeronikaSzemi <weronikaszeminska@gmail.com>
2023-04-12 18:07:10 +0200

    added config example and moved DB connection data into config

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,17 +9,18 @@ import { AuthModule } from './auth/auth.module';
 import { TermbaseModule } from './termbase/termbase.module';
 import { Termbase } from './termbase/entities/termbase.entity';
 import { EntryModule } from './entry/entry.module';
-import { Entry } from "./entry/entities/entry.entity";
+import { Entry } from './entry/entities/entry.entity';
+import { config } from '../config/config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: config.dbHost,
       port: 3306,
-      username: 'root',
-      password: '',
-      database: 'termino',
+      username: config.dbUser,
+      password: config.dbPassword,
+      database: config.dbDatabase,
       entities: [User, Termbase, Entry],
       bigNumberStrings: false,
       logging: true,
